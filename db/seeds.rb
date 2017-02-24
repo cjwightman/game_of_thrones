2eae90c7484053680e75c74e4a52d0bc61f757d3
Chris Wightman <wightman_christopher@bah.com>
2017-02-24 10:32:20 -0500

    Seed file created and ingested

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+stark = House.create(manor: "Stark" )
+targaryan = House.create(manor: "Targaryan")
+lannister = House.create(manor: "Lannister")
+Character.create(name: "Eddard Stark", house: stark, img_url: "http://bit.ly/2lRLGfY")
+Character.create(name: "Robb Stark", house: stark, img_url: "http://bit.ly/2msMyEy")
+Character.create(name: "Tyrion Lannister", house: lannister, img_url: "http://bit.ly/2l7J4pN")
+Character.create(name: "Tywin Lannister", house: lannister, img_url: "http://bit.ly/2l7LG71")
+Character.create(name: "Jon Snow", house: targaryan, img_url: "http://gameofthrones.wikia.com/wiki/Jon_Snow?file=Battle_of_the_Bastards_08.jpg")
+Character.create(name: "Benjen Stark", house: targaryan, img_url: "http://bit.ly/2lAq2Mi")
